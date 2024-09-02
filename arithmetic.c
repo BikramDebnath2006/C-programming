510fcf96c029b3f61d4700c0c3bd62938b8623c4
Bikram Debnath <bikramdebnath.15092.9a.22@gmail.com>
2024-09-02 22:17:11 +0530

    c program to solve arithmetic problems

---
+++ arithmetic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a, b,sum,mul;
+    printf("enter the first number:");
+    scanf("%d",a);
+    printf("enter  the second number:");
+    scanf("%d",b);
+    sum=a+b;
+    mul=a*b;
+    printf("the adition and multiplication of the two numbers is:%d ", sum);
+    return 0;
+}
