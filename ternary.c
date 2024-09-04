b7a4a353427862b7a21704d30b61175e30ac07aa
Bikram Debnath <bikramdebnath.15092.9a.22@gmail.com>
2024-09-04 20:48:25 +0530

    ternary

---
+++ ternary.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("enter age of the canditate");
+    scanf("%d",&age);
+
+    age>=28? printf("adult\n"):printf("not adult\n");
+    return 0;
+}
